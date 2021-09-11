96865c5bf6c703064602b6573cc53d93a16c5bb9
Abraao Lincol <abraao.lincol@gmail.com>
2021-09-11 16:51:33 -0300

    add formulario de cadastro de servico

--- map.js
+++ map.js
@@ -1,14 +1,29 @@
 window.onload = () => {
-    //loadMap();
+    loadMap();
 }
 
 function loadMap() {
-    var map = L.map('map').setView([-5.79448, -35.211], 13);
+    var map = L.map('map').setView([-5.744178761920199, -35.20799362887624], 15);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
-    var marker = new L.marker([-5.79448, -35.211]).addTo(map);
-    marker = new L.marker([-5.79148, -35.211]).addTo(map);
+    var marker = new L.marker([-5.744178761920199, -35.20799362887624]).bindTooltip("<b>Hello world!</b>", {permanent: true,  offset: [-65, -32] }).on('dblclick', x).addTo(map);
+    marker = new L.marker([-5.79148, -35.211]).bindTooltip("<b>Hello world!</b>", {permanent: true,  offset: [-65, -32] }).addTo(map);
+    map.dragging.disable();
+}
+
+function x() {
+    console.log("teste");
+}
+
+function showFormService() {
+    document.getElementById("form-service").classList.remove("d-none");
+    document.getElementById("form-service").classList.add("d-flex");
+}
+
+function hideFormService() {
+    document.getElementById("form-service").classList.remove("d-flex");
+    document.getElementById("form-service").classList.add("d-none");
 }
